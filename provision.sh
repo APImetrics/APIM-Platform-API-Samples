76f1501e9642e7d7769f8c5ec9386ad20f7e317c
u2000 <ambrown@gmail.com>
2022-08-31 10:39:29 -0400

    Add API call now functional

--- provision.sh
+++ provision.sh
@@ -2,10 +2,29 @@
 
 
 ## To pretty-print JSON results, specify query parameter "debug=1".
+# url="https://google.api.expert/get"
+# method="GET"
+# params="solid, liquid, gas"
+# call_name="MyNewAPICall"
 
 source ./setenv.sh
 
 echo using API key: $api_key
 
+echo "Enter the full URL (including https:// or https://) for your API call $url, followed by [ENTER] specify"
 
-curl -v https://client.apimetrics.io/api/2/calls/"?_token=$api_key&debug=1"
+read url
+
+echo "Enter the HTTP method for your API call $method, followed by [ENTER]:"
+
+read method
+
+echo "Enter any parameters for your API call $params, followed by [ENTER]:"
+
+read params
+
+echo "Enter a name for your API call $call_name, followed by [ENTER]:"
+
+read call_name
+
+curl -v -X PUT -H 'Accept: application/json' -H 'Content-Type: application/json' https://client.apimetrics.io/api/2/calls/"?_token=$api_key&debug=1" --data '{ "meta": { "name":"'"$call_name"'" },"request": { "method":"'"$method"'" , "url":"'"$url"'" }}'
