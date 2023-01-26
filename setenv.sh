48ac9bb58deb2564d81b298c2cad9f52e0e388d6
u2000 <ambrown@gmail.com>
2023-01-26 11:18:56 -0500

    adding sample project YAML

--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-## Update below section for your environment
+## Update below section for your environment on APImetrics.io
 ## --------------------------------------
 
 email="The email for your APImetrics account"
-api_key="Your API ley here"
+#https://client.apimetrics.io/settings/api-key
+api_key="ot1JfvV7lWqwO9nIbX05TzlICjziPjnd"
 call_id="Your API call ID here--add this after you create your first API call"
 
 ## Do not change the settings below
 ## --------------------------------------
 export api_key=$api_key
-export call_id=$call_id
+export call_id=$call_id	
